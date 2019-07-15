9063d57739cf3ca45f753adfb61d0e9f19eabd55
prabhat158 <prabhatov158@gmail.com>
2019-07-15 21:23:41 +0530

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompetitionComponent } from './competition/competition.component';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompetitionComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'',component:HomeComponent},
+      {path:'competition',component:CompetitionComponent},
+      {path:'contact',component:ContactComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
