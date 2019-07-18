8d46eccd18db1d8a1ebdbc21bed1d967992d2576
prabhat158 <prabhatov158@gmail.com>
2019-07-19 04:05:58 +0530

    contact90%

---
+++ src/app/organs.ts
@@ -0,0 +1,329 @@
+export const organs = [
+	{
+		body:'COMPETITIONS & LYP',
+		id:'comp',
+		members:[
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+
+
+		]
+	},
+
+
+	{
+		body:'CREATIVES',
+		id:'creatives',
+		members:[
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			}
+		]
+	},
+
+
+	{
+		body:'FOOD AND BEVERAGES',
+		id:'f&b',
+		members:[
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			
+		]
+	},
+
+
+	{
+		body:'HORIZONS & WORKSHOPS',
+		id:'h&w',
+		members:[
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			
+		]
+	},
+
+	{
+		body:'HOSPITALITY, ASSISTANCE',
+		id:'hospi',
+		members:[
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			
+		]
+	},
+
+	{
+		body:'INFORMALS',
+		id:'informal',
+		members:[
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			
+		]
+	},
+
+	
+	{
+		body:'MARKETING',
+		id:'mark',
+		members:[
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			
+		]
+	},
+
+	{
+		body:'MEDIA & PUBLICITY',
+		id:'m&p',
+		members:[
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			
+		]
+	},
+
+	{
+		body:'PRONITES',
+		id:'pro',
+		members:[
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			
+		]
+	},
+
+
+	{
+		body:'LOGISTICS & OPERATIONS',
+		id:'l&o',
+		members:[
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			{
+				name:'Prabhat Singh',
+				img:'../assets/images/img_avatar.png',
+				fb:'',
+				in:'',
+				insta:'',
+				twitter:'',
+				mail:'prabhat@moodi.org',
+				phno:'+918115618738',
+
+			},
+			
+		]
+	},
+	
+];
