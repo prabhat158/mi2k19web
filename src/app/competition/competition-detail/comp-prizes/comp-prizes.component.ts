31849f2b362eec508508d01aa1951d05a1e1de46
garg-saurav <garg.saurav08@gmail.com>
2019-07-20 18:31:06 +0530

    completed individual events page: 95%

--- src/app/competition/competition-detail/comp-prizes/comp-prizes.component.ts
+++ src/app/competition/competition-detail/comp-prizes/comp-prizes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CompetitionDataService } from '../../competition-data.service';
 
 @Component({
   selector: 'app-comp-prizes',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompPrizesComponent implements OnInit {
 
-  constructor() { }
+  competitions_categories: any[];
+  current_competition_category: string;
+  current_event: string;
+  
+  constructor(
+    private route: ActivatedRoute,
+    private dataService: CompetitionDataService
+  ) { }
+
+  getCategories(){
+    this.dataService.getCategories()
+      .subscribe(
+        data=> {  
+          this.competitions_categories = data['Competitions'];
+        }
+    );
+  }  
 
   ngOnInit() {
+    this.getCategories();
+    this.route.parent.params.subscribe(
+      params => {
+        this.current_competition_category = params.compiName,
+        this.current_event = params.eventName 
+      }
+    );
   }
 
 }
--- src/app/competition/competition.component.ts
+++ src/app/competition/competition.component.ts
@@ -12,6 +12,7 @@ export class CompetitionComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.clickedButton='';
   }
 
   onClick(button){
