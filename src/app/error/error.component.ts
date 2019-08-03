f96ba5d645ddee39b30432c78a498109dde78ded
prabhat158 <prabhatov158@gmail.com>
2019-08-03 18:25:47 +0530

    error

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-// import * as $ from 'jquery';
+import * as $ from 'jquery';
+// declare var $:JQueryStatic;
 
 @Component({
   selector: 'app-error',
@@ -11,11 +12,14 @@ export class ErrorComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).on("click mousemove",".box",function(e){ 
+  var x = e.clientX;
+  var y = e.clientY;
+  var newposX = x - 60;
+  var newposY = y - 60; 
+  $(".circle").css("transform","translate3d("+newposX+"px,"+newposY+"px,0px)");
+  });
   }
-  onMouseMove = (event: MouseEvent) => {
-    
-  }
-
 
 }
 // $(document).on("click mousemove",".box",function(e){ 
